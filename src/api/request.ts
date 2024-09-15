0662ab74f28d83612b02352adf22dfe06edc04be
bruno_yu <bruno_yu@wonder4studio.com>
2024-09-16 07:15:26 +0800

    fix: 修正 request CustomFetchWrapper 中 env api 地址

--- src/api/request.ts
+++ src/api/request.ts
@@ -52,7 +52,7 @@ class CustomFetchWrapper {
 const apiService = new CustomFetchWrapper({
   baseURL: import.meta.env.DEV
     ? `${import.meta.env.VITE_API_PREFIX}`
-    : `${import.meta.env.VITE_API_URL}`,
+    : `${import.meta.env.VITE_APP_API}`,
   // baseURL: import.meta.env.VITE_API_URL,
   headers: { 'Content-Type': 'application/json' },
 })
